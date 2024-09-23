a2feaeeb582171792712f852c0b631f92faf486b
tasfia8 <117693390+tasfia8@users.noreply.github.com>
2024-09-23 18:34:31 -0400

    Update hello.py Activity1.2 exp2-2 T1

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
-#Example 2-1. hello.py: A complete Flask application
+#Example 2-2. hello.py: Flask application with a dynamic route
 
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index():
- return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+ return '<h1>Hello, {}!</h1>'.format(name)
