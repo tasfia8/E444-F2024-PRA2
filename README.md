c5e176bcfb0efd173fe8820c6ad5feb1ef4f2f16
tasfia8 <tasfia8@yahoo.ca>
2024-09-24 01:30:56 -0400

    Activity 1.4 steps 1-5 Completed

--- README.md
+++ README.md
@@ -41,3 +41,36 @@ then do flask run
 Then go to http://127.0.0.1:5000/
 
 ![](Screenshots/A1_4p1c.png)
+
+## Activity 1.4: step 2 - Email, Name - Hello, Stranger!
+
+Alongside installing, running flask in Activity 1.4 had to install flask-wtf wtforms, email-validators otherwise import doesn't work.
+
+After installing flask within the active virtual environment, then do : pip install Flask-WTF WTForms
+then do pip install email-validator
+
+Optional step: to verify installation: pip freeze
+
+Then do flask run
+
+![](Screenshots/A1_4step2a.png)
+
+![](Screenshots/A1_4step2b.png)
+
+## Activity 1.4: step 3 
+Fill in your first name and your UofT email address, click submit
+
+![](Screenshots/A1_4step3a.png)
+
+## Activity 1.4: step 4
+Fill in your first name and last name in the 1st field, and fill in your first name in the 2nd field, click submit. You will receive an error message 
+
+![](Screenshots/A1_4step4aInvalid.png)
+
+## Activity 1.4: step 5 (Final Screenshot)
+Fill in your first name and last name in the 1st field, and fill in your NON UofT email in the 2nd field, click submit. (Take  a screenshot)
+
+![](Screenshots/A1_4step5a.png)
+
+Extra:
+![](Screenshots/A1_4step5b.png)
--- hello.py
+++ hello.py
@@ -5,41 +5,65 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
 
-#A4-C1
+#A1_4-C1
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import Email, DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string' #A4-C3
+app.config['SECRET_KEY'] = 'hard to guess string' #A1_4-p1 C3
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-#A4-C2
+#A1_4p1-C2
 class NameForm(FlaskForm):
  name = StringField('What is your name?', validators=[DataRequired()])
+ #A1_4p2 C1
+ email=StringField('What is your UofT Email address?', validators=[DataRequired(),Email()])
+ 
  submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+  
  
     if form.validate_on_submit():
         old_name = session.get('name')
+        email_prev = session.get('email')
+
+        #If user change their name
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+    #Email verify
+        
+        if 'utoronto' not in form.email.data:
+            flash('Looks like you have changed your email!')
+
+            #If user inputs invalid email, then session clears
+            session['email'] = None
+            return render_template('index.html', form=form, name=form.name.data, email=None)
+
+            #return redirect(url_for('index'))
+
+        #If user change their name
+        if email_prev is not None and email_prev != form.email.data:
+            flash('Looks like you have changed your name!')
+        
+        #Keeping the email, name in storage session
         session['name'] = form.name.data
+        session['email'] = form.email.data
+        
         return redirect(url_for('index'))
-    return render_template('index.html',
-        form = form, name = session.get('name'))
+    
+    return render_template('index.html', form = form, 
+                           name = session.get('name'), email = session.get('email'))
 
 
-
-
-
-#@app.route('/user/<name>')
-#def user(name):
-# return render_template('user.html', name=name)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 #Example 3-6. hello.py: custom error pages
 
