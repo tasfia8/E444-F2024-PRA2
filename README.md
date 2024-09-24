e058acc8b942bc1d29ab02c7b052864fb736e95a
tasfia8 <tasfia8@yahoo.ca>
2024-09-23 23:14:48 -0400

    Activity 1.4 step 1 reproduced exp4-7 completefinal

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 
 ### Activity 1.2: Creating a repo and replay the textbook example
 
+Install Flask, run flask within ECE444-F2024-PRA2 following instructions:
+https://flask.palletsprojects.com/en/3.0.x/installation/
+
 ## Activity 1.2: Example 2-1
 ![](Screenshots/Pra2Activity1_2.png)
 
@@ -22,3 +25,19 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 
 ![](Screenshots/A1p3b.png)
 
+### Activity 1.4: Replaying and modifying example in Chapter 4
+
+## Activity 1.4: step 1 - Example 4-7 (Hello Stranger, What is your name?)
+
+Alongside installing, running flask in Activity 1.4 had to install flask-wtf wtforms otherwise import doesn't work
+
+After installing flask within the active virtual environment, then do : pip install Flask-WTF WTForms
+then do flask run
+
+![](Screenshots/A1_4p1a.png)
+
+![](Screenshots/A1_4p1b.png)
+
+Then go to http://127.0.0.1:5000/
+
+![](Screenshots/A1_4p1c.png)
--- hello.py
+++ hello.py
@@ -1,21 +1,45 @@
 #Example 3-3. hello.py: rendering a template
-from flask import Flask, render_template
+
+from flask import Flask, render_template, session, flash, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
 
+#A4-C1
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string' #A4-C3
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+#A4-C2
+class NameForm(FlaskForm):
+ name = StringField('What is your name?', validators=[DataRequired()])
+ submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
- return render_template('index.html', current_time=datetime.now(timezone.utc))
+    form = NameForm()
+ 
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'))
 
-@app.route('/user/<name>')
-def user(name):
- return render_template('user.html', name=name)
+
+
+
+
+#@app.route('/user/<name>')
+#def user(name):
+# return render_template('user.html', name=name)
 
 #Example 3-6. hello.py: custom error pages
 
